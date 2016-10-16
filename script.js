e51fb42920f0624ed0a3bab6e5f3b0f89e26a557
femarchi <fepmarchi@gmail.com>
2016-10-16 19:59:20 -0200

    Working version of quote generator

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
 $(document).ready(function() {
 	
-	// Function calls when opening the page
-
-
-	// function loadDoc() {
-	// 	// Load all quotes from txt file into an object
- //  	var xhttp = new XMLHttpRequest();
- //  	xhttp.onreadystatechange = function() {
- //  	  if (this.readyState == 4 && this.status == 200) {
- //  	   var quoteString = this.responseText;
- //  	   //Create new objects with fields: id(int), Quote(string), Author(string)
- //  	   //Do this by splitting the array for every new line and pushing the values in order
-
- //  	  }
- //  	};
- //  	xhttp.open("GET", "quote-list.txt", true);
- //  	xhttp.send();
-	// }
-
 	function loadQuote (quoteId) {
 		//fetch a random quote by id number and insert it into the html
-		
-		var loadedQuote = "Dummy Quote";
-		var loadedAuthor = "Jason Ajax";
 
-		$.getJSON("quote-list.json", function (data){
+		$.getJSON("https://raw.githubusercontent.com/femarchi/random-quote-machine/master/quote-list.json", function (data){
 			var i = 0;
 			var found = false;
+			
 			while (!found && i < data.length){
-				if(data[i].id === quoteId){
-					alert("Found " + quoteId + "!");
-					loadedQuote = data[i].quote;
-					loadedAuthor = data[i].author;
+				if(data[i].ID === quoteId){
+					//alert("Found " + quoteId + "!");
+					console.log(
+											data[i].ID + "\n"
+											+ data[i].quote + "\n"
+											+ data[i].author
+											);
+					$("#quote").html(data[i].quote);
+					$("#author").html(data[i].author);
 					found = true;
 				}
 				i++;
 			}
+
 		});
 
-		$("#quote").html(loadedQuote);
-		$("#author").html(loadedAuthor);
-
-		//document.getElementById("quote").innerHTML = "Quote loaded";
-		//document.getElementById("author").innerHTML = "Author";
 	}
 
 	$("#new-quote-button").on("click", function(){
