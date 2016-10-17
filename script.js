4332f0b250ed13235748ec14e925995396645df6
femarchi <fepmarchi@gmail.com>
2016-10-17 17:15:12 -0200

    Added some styling and bg color change

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
 	
-	function loadQuote (quoteId) {
+	//keep track of current status to avoid repetition (to implement)
+	var currentID = 0;
+	var currentColor = "";
+
+	var bgColor = function() {
+		//generate a new background color
+		var colorArray = ["lightgray", "gray", "darkgray", "black"];
+		var newColor = colorArray[Math.floor(Math.random()*(colorArray.length))];
+		$("#page-wrapper").css("background-color", newColor);
+		currentColor = newColor;
+	}
+
+	function loadQuote () {
 		//fetch a random quote by id number and insert it into the html
 
 		$.getJSON("https://raw.githubusercontent.com/femarchi/random-quote-machine/master/quote-list.json", function (data){
 			var i = 0;
 			var found = false;
+			var quoteId = Math.floor(Math.random()*data.length);
+			//console.log(currentID);
 			
 			while (!found && i < data.length){
 				if(data[i].ID === quoteId){
-					//alert("Found " + quoteId + "!");
-					console.log(
-											data[i].ID + "\n"
-											+ data[i].quote + "\n"
-											+ data[i].author
-											);
+					// console.log(
+					// 						data[i].ID + "\n"
+					// 						+ data[i].quote + "\n"
+					// 						+ data[i].author
+					// 						);
 					$("#quote").html(data[i].quote);
 					$("#author").html(data[i].author);
+					currentID = quoteId;
 					found = true;
 				}
 				i++;
@@ -27,10 +41,9 @@ $(document).ready(function() {
 	}
 
 	$("#new-quote-button").on("click", function(){
-		// Load a new quote from the object by clicking the button
-		
-		var quoteId = Math.floor(Math.random()*100);
-		loadQuote(quoteId);
+		// button click changes quote and bgcolor
+		loadQuote();
+		bgColor();
 	});
 
 });
